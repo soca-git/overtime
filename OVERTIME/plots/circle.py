ea6d6b95912eba54d0eb25d26d4d0e2c59ad3653
soca-git <soca.mailbox@gmail.com>
2020-07-17 14:55:19 +0100

    updated plots

--- OVERTIME/plots/circle.py
+++ OVERTIME/plots/circle.py
@@ -1,7 +1,8 @@
 
-import numpy as np
 import math
+import numpy as np
 import matplotlib.pyplot as plot
+import matplotlib.lines as lines
 
 class Circle:
     """
@@ -19,22 +20,102 @@ class Circle:
     def create(self):
         graph = self.graph
         nodes = self.nodes
+        edges = self.edges
         n = graph.nodes.count()
+
         i = 0
         for node in graph.nodes.set:
             nodes[node.label] = {}
             nodes[node.label]['x'] = math.cos(2 * math.pi * i / n)
             nodes[node.label]['y'] = math.sin(2 * math.pi * i / n)
-            self.ax.scatter(
-                nodes[node.label]['x'], nodes[node.label]['y'], s=500, zorder=1
-            )
+            i += 1
+            
+        for edge in graph.edges.stream:
+            edges[edge.uid] = {}
+            edges[edge.uid]['p1'] = {}
+            edges[edge.uid]['p1']['x'] = nodes[edge.source.label]['x']
+            edges[edge.uid]['p1']['y'] = nodes[edge.source.label]['y']
+            edges[edge.uid]['p2'] = {}
+            edges[edge.uid]['p2']['x'] = nodes[edge.sink.label]['x']
+            edges[edge.uid]['p2']['y'] = nodes[edge.sink.label]['y']
+        
+        self.draw_nodes(graph, nodes, edges, n)
+        self.draw_edges(graph, nodes, edges)
+        self.cleanup()
+
+
+    def draw_nodes(self, graph, nodes, edges, n):
+        pos = {}
+        pos['x'] = [nodes[key]['x'] for key in nodes.keys()]
+        pos['y'] = [nodes[key]['y'] for key in nodes.keys()]
+
+        colors = self.colors(n)
+        ax_node = self.ax.scatter(
+            pos['x'], pos['y'], s=500, c=colors, cmap='viridis', zorder=1
+        )
+        plot.draw()
+
+        i = 0
+        for node in self.graph.nodes.set:
+            nodes[node.label]['color'] = ax_node.to_rgba(colors[i])
             self.ax.text(
                 nodes[node.label]['x']-0.025, nodes[node.label]['y']-0.025,
                 node.label, color='white'
             )
             i += 1
 
-        self.cleanup()
+
+    def colors(self, n):
+        c = [(1/n * x) for x in range(0, n)]
+        c_mix = [0.0]
+        for i in range(1,n):
+            if i % 2 == 0:
+                c_mix.append(c[i-1])
+            else:
+                c_mix.append(c[n-i])
+        return c_mix
+
+
+    def draw_edges(self, graph, nodes, edges):
+        for edge in graph.edges.stream:
+            bezier = self.bezier(
+                edges[edge.uid]['p1'],
+                edges[edge.uid]['p2']
+            )
+            self.ax.plot(
+                bezier['x'],
+                bezier['y'],
+                linestyle='-',
+                color=nodes[edge.source.label]['color'],
+                zorder=0
+            )
+            self.ax.plot(
+                bezier['x'][4],
+                bezier['y'][4],
+                'o',
+                color=nodes[edge.source.label]['color'],
+                zorder=1
+            )
+            self.ax.text(
+                bezier['x'][6], bezier['y'][6],
+                edge.time, color='black', backgroundcolor='white',
+                zorder=1
+            )
+
+
+    def bezier(self, p1, p2, p0=(0,0), nt=20):
+        bezier = {}
+        bezier['x'] = []
+        bezier['y'] = []
+        for i in range(0, nt+1):
+            t = (1/nt) * i
+            bezier['x'].append(
+                (p1['x']-2*p0[0]+p2['x'])*math.pow(t,2) + 2*t*(p0[0]-p1['x']) + p1['x']
+            )
+            bezier['y'].append(
+                (p1['y']-2*p0[0]+p2['y'])*math.pow(t,2) + 2*t*(p0[0]-p1['y']) + p1['y']
+            )
+        return bezier
 
 
     def cleanup(self):
