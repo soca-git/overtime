af8f08a28abe394d86ed6195a738591de43b8ac1
soca-git <soca.mailbox@gmail.com>
2020-07-20 23:45:09 +0100

    updated examples

--- OVERTIME/plot_gif_example.py
+++ OVERTIME/plot_gif_example.py
@@ -12,16 +12,14 @@ input_data = CSVInput('network', 'csv', './network.csv')
 graph = TemporalGraph('TestNetwork', input_data)
 graph.print()
 
-figure, axes = plt.subplots(1)
-Circle(graph, axes)
+labels = []
+for t in graph.edges.timespan():
+    figure, axes = plt.subplots(1)
+    labels.append('time_' + str(t) + '.png')
+    Circle(graph.get_graph_by_time(t), axes, t)
+    figure.savefig(labels[-1], format='png')
 
-figure, axes = plt.subplots(nrows=2, ncols=2)
-foremost_a = CalculateForemostTree(graph, 'a')
-Circle(foremost_a.tree, axes[0][0])
-
-foremost_c = CalculateForemostTree(graph, 'c')
-Circle(foremost_c.tree, axes[0][1])
-
-plt.show()
+images = [imageio.imread(f) for f in labels]
+imageio.mimsave('network.gif', images, duration=2)
 
 input("Press enter key to exit...")
