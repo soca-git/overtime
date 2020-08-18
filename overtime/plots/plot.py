d38abc63cf89ecab6d94f699901f81abae93e44b
soca-git <soca.mailbox@gmail.com>
2020-08-18 15:21:12 +0100

    PEP8 updates

--- overtime/plots/plot.py
+++ overtime/plots/plot.py
@@ -82,36 +82,78 @@ class Plot:
 
 
     def update_name(self):
+        """
+            A method of Plot.
+
+            Returns:
+            --------
+                None, combines the graph label and plot class name
+                and updates self.name for the plot.
+        """
         name = self.class_name + '-' + self.graph.label
+        # replace is used to filter characters that are not supported in file names.
         self.name = name.replace(' ', '_').replace(':','').replace(',','')
 
 
     def create(self):
-        self.create_nodes()
-        self.create_edges()
+        """
+            A method of Plot.
 
-
-    def draw(self):
-        if self.title is not None:
-            self.draw_figure()
-        self.draw_nodes()
-        self.draw_edges()
-        self.cleanup()
+            Returns:
+            --------
+                None, creates plot's nodes & edges.
+        """
+        self.create_nodes() # create the plot's nodes.
+        self.create_edges() # create the plot's edges.
 
 
     def create_nodes(self):
+        """
+            A method of Plot.
+                Base method to be customized for each Plot subclass.
+                Creates plot's nodes.
+        """
         pass
 
 
     def create_edges(self):
+        """
+            A method of Plot.
+                Base method to be customized for each Plot subclass.
+                Creates plot's edges.
+        """
         pass
 
 
-    def draw_figure(self):
-        title = '' if self.title is None else self.title
-        if not self.graph.static:
-            title = title + ' [time: ' + str(self.graph.edges.start()) + ']'
+    def draw(self):
+        """
+            A method of Plot.
 
+            Returns:
+            --------
+                None, draws plot's title, nodes & edges and cleans up figure & axis.
+        """
+        if self.title is not None:
+            self.draw_title() # draw title.
+        self.draw_nodes() # draw the plot's nodes.
+        self.draw_edges() # draw the plot's edges.
+        self.cleanup() # cleanup the figure & axis.
+
+
+    def draw_title(self):
+        """
+            A method of Plot.
+
+            Returns:
+            --------
+                None, draws plot title.
+        """
+        title = '' if self.title is None else self.title
+        # if the graph is temporal.
+        if not self.graph.static:
+            # add the graph timespan to the plot title.
+            title = title + ' [time: ' + str(self.graph.edges.timespan()) + ']'
+        # draw the title.
         self.axis.set_title(
             label=title,
             loc='center'
@@ -119,18 +161,48 @@ class Plot:
 
     
     def draw_nodes(self):
+        """
+            A method of Plot.
+                Base method to be customized for each Plot subclass.
+                Draws plot's nodes.
+        """
         pass
 
 
     def draw_edges(self):
+        """
+            A method of Plot.
+                Base method to be customized for each Plot subclass.
+                Draws plot's edges.
+        """
         pass
 
 
     def cleanup(self):
+        """
+            A method of Plot.
+                Base method to be customized for each Plot subclass.
+                Cleans up figure & axis.
+        """
         pass
 
 
     def set3colormap(self, n):
-        n = math.ceil(n/12)
+        """
+            A method of Plot.
+            Parameter(s):
+            -------------
+            n : Integer
+                Number of objects to be assigned a color.
+            
+            Returns:
+            --------
+            cmap : ListedColorMap
+                A pyplot ListedColorMap object.
+                Map has enough colors to assign to n objects without color adjacency.
+        """
+        n = math.ceil(n/12) # Set3 cmap has 12 colours, divide n and ceil.
         cmap = cm.get_cmap('Set3')
+        # Return an expanded Set3 cmap with enough repeating colors
+        # to cover the number of objects to be drawn
         return ListedColormap(cmap.colors*n)
