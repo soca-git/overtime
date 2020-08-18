58f22657d424d054da9ca69590042bad9c274ba2
soca-git <soca.mailbox@gmail.com>
2020-08-18 16:24:43 +0100

    PEP8 updates

--- overtime/plots/slice.py
+++ overtime/plots/slice.py
@@ -123,14 +123,19 @@ class Slice(Plot):
             --------
                 None, creates the SliceEdge objects.
         """
-        step = 1
+        step = 1 # step multiplier.
+        # for each unique edge label (independent of time).
         for label in self.graph.edges.ulabels():
-            self.labels.append(label)
+            self.labels.append(label) # append the edge label to the plot labels list.
         
+        # for each timestep in the graph's timespan.
         for t in self.graph.edges.timespan():
+            # for every edge in the graph.
             for edge in self.graph.edges.set:
+                # if the edge is active at the 't'.
                 if edge.isactive(t):
-                    i = self.labels.index(edge.label)
+                    i = self.labels.index(edge.label) # get the index of the edge within self.labels.
+                    # create a SliceEdge and add it to the edges list.
                     self.edges.append(SliceEdge(edge, i, x=(t*step), y=(i*step)))
         
 
@@ -143,10 +148,11 @@ class Slice(Plot):
                 None, draws the edges of the plot.
         """
         pos = {}
-        pos['x'] = [edge.x for edge in self.edges]
-        pos['y'] = [edge.y for edge in self.edges]
-        colors = pos['y']
-        cmap = self.set3colormap(self.graph.edges.count())
+        pos['x'] = [edge.x for edge in self.edges] # x coordinates of every node.
+        pos['y'] = [edge.y for edge in self.edges] # y coordinates of every node.
+        colors = pos['y'] # colors index for every edge.
+        cmap = self.set3colormap(self.graph.edges.count()) # color map with enough colors for n edges.
+        # draw the edges using pyplot scatter.
         self.axis.scatter(
             pos['x'], pos['y'], s=50, c=colors, cmap=cmap, zorder=1
         )
@@ -224,35 +230,54 @@ class Slice(Plot):
                 None, draws sliders for the x-axis, y-axis, or both.
         """
         flag = False
+        # if the plot has sliders enabled and the number of x-ticks is deemed large.
         if self.has_slider and len(xticks) > xstep:
+            # set x-axis range.
             self.axis.set(xlim=(xticks[0]-1, xstep))
-            axpos = plt.axis([0.325, 0.035, 0.55, 0.025])
+            axpos = plt.axis([0.325, 0.035, 0.55, 0.025]) # slider bbox position.
+            # Create x-axis pyplot slider.
             xpos = Slider(axpos, 'Time', xticks[0]-1, xticks[-1]-xstep, color='aquamarine')
+            # Update function for changes in x.
             def updatex(val):
-                pos = xpos.val
+                pos = xpos.val # position of slider (clickable).
+                # maximum x-tick (influenced by figure window size).
                 xtick_max = 4+pos+math.floor(self.figure.get_size_inches()[0]*4)
+                # update x-axis range.
                 self.axis.set(xlim=(pos, xtick_max))
-                self.figure.canvas.draw_idle()
+                self.figure.canvas.draw_idle() # draw updates.
+            
+            # on clicking slider, update axis.
             xpos.on_changed(updatex)
-            updatex((xticks[0]-1, xstep))
+            updatex((xticks[0]-1, xstep)) # initial update.
+            # on figure window resize, update axis.
             self.figure.canvas.mpl_connect('resize_event', updatex)
-            flag = True
+            flag = True # flag a slider was created.
         
+        # if the plot has sliders enabled and the number of y-ticks is deemed large.
         if self.has_slider and len(yticks) > ystep:
+            # set y-axis range.
             self.axis.set(ylim=(yticks[0]-1, ystep))
-            axpos = plt.axis([0.025, 0.25, 0.0125, 0.6])
+            axpos = plt.axis([0.025, 0.25, 0.0125, 0.6]) # slider bbox position.
+            # Create y-axis pyplot slider.
             ypos = Slider(axpos, 'Edge', yticks[0]-1, yticks[-1]-ystep, orientation='vertical', color='mediumspringgreen')
+            # Update function for changes in y.
             def updatey(val):
-                pos = ypos.val
+                pos = ypos.val # position of slider (clickable).
+                # maximum y-tick (influenced by figure window size).
                 ytick_max = 4+pos+math.floor(self.figure.get_size_inches()[1]*4)
+                # update y-axis range.
                 self.axis.set(ylim=(pos, ytick_max))
-                self.figure.canvas.draw_idle()
+                self.figure.canvas.draw_idle() # draw updates.
+            
+            # on clicking slider, update axis.
             ypos.on_changed(updatey)
-            updatey((yticks[0]-1, ystep))
+            updatey((yticks[0]-1, ystep)) # initial update.
             self.figure.canvas.mpl_connect('resize_event', updatey)
-            flag = True
+            # on figure window resize, update axis.
+            flag = True # flag a slider was created.
 
+        # if a slider was created, we need to run plt.show() now to allow user interaction.
         if flag:
             print('plt.show() activated. Please close the figure(s) in order to continue.')
-            self.show = False
+            self.show = False # plotter no longer needs to call figure.show()
             plt.show()
